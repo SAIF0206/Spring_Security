5b92b77a36d5fe1c44b6edd77ca5a7c5dadc7741
Saif <msaif.sam@gmail.com>
2021-02-23 20:17:07 +0800

    Initial commit

---
+++ src/main/java/com/example/security/students/Student.java
@@ -0,0 +1,26 @@
+package com.example.security.students;
+
+public class Student {
+
+    //alt+enter
+
+    private final Integer studentId;
+    private final String studentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    //alt+enter
+
+
+}
